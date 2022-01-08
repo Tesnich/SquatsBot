a5af3b1d45c74d264507fb273fd19605836b66c4
James <zacherysparks53@gmail.com>
2022-01-08 13:31:14 -0500

    Add stop notify, add schedule event, change casing

--- squatsbot-NOKEY.py
+++ squatsbot-NOKEY.py
@@ -19,16 +19,6 @@ class CustomClient(discord.Client):
     #///////////////////////////////////////////////////////////////////////////////////
     #   Method Run On Bot Connection To Discord Servers
     async def on_ready(self):
-        today = datetime.date.today()
-        now = datetime.datetime.now()
-        startTime = now.strftime("%H:%M:%S")
-        print("Today's date:", today)
-        print("Start Time : ", startTime)
-        # dates = list()
-        # dates.append(today.isoformat())
-        # for i in range(30):
-        #     dates.append((today + datetime.timedelta(i)).isoformat())
-        # print (dates)
         print(str(client.user) + ' has connected to Discord!')
         for guild in client.guilds:
             print('Connected to ... ' + str(guild)+'!')
@@ -40,7 +30,6 @@ class CustomClient(discord.Client):
                 with open (str(guild)+'.csv', 'w') as csvIn: #will associating the guild name with file storage be a problem? What if server is renamed ... fix it later?
                     filewriter = csv.writer(csvIn)
                     filewriter.writerow(['Server', str(guild)])
-                    filewriter.writerow(['Date', '2021-12-15', '2021-12-16', '2021-12-17', '2021-12-18', '2021-12-19', '2021-12-20', '2021-12-21', '2021-12-22', '2021-12-23', '2021-12-24', '2021-12-25', '2021-12-26', '2021-12-27', '2021-12-28', '2021-12-29', '2021-12-30', '2021-12-31', '2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05', '2022-01-06', '2022-01-07', '2022-01-08', '2022-01-09', '2022-01-10', '2022-01-11', '2022-01-12', '2022-01-13', '2022-01-14'])
                     filewriter.writerow(['Standard',10,15,20,25,0,30,35,40,45,0,50,55,60,65,0,70,75,80,85,0,90,95,100,105,0,110,115,120,125,130])
                     filewriter.writerow(['Hard',50,55,60,0,70,75,80,0,100,105,110,0,130,135,140,0,150,155,160,0,180,185,190,0,220,225,230,0,240,250])
                     filewriter.writerow(['USERS'])
@@ -61,14 +50,14 @@ class CustomClient(discord.Client):
         
         #///////////////////////////////////////////////////////////////////////////////////
         #   Help text/ explainer
-        if message.content == '!SquatsBot Help':
-            response = "Looks like you want to get swole... **BEHOLD THY COMMANDS!**\n----------------------------------------------------------------------------\n!SquatsBot Swoletime - Sign up for daily Squatifications!\n!SquatsBot Swolechief Managed - End your daily Squatifications.\n!SquatsBot Start - Start bot notifications! The time at which this command is sent will be the time participants will get a reminder each day!\n----------------------------------------------------------------------------\nPS: Async discord tasks are new to me, so I have no idea how to shut them off yet. Don't start the challenge more than once.\nIf you need terminate functionality tell me and I'll just kill the script host. - Tesnich"
+        if message.content == '!squatsbot help':
+            response = "Looks like you want to get swole... **BEHOLD THY COMMANDS!**\n----------------------------------------------------------------------------\n!squatsbot signup - Sign up for daily Squatifications!\n!squatsbot swolechief managed - End your daily Squatifications.\n!squatsbot schedule event - schedule a new 30 day event starting now! (replaces old schedule)\nsquatsbot start notify - Start bot notifications! The time at which this command is sent will be the time participants will get a reminder each day!\n!squatsbot stop notify - Stop daily reminders.\n----------------------------------------------------------------------------\nPS: Async discord tasks are new to me, so I have no idea how to shut them off yet. Don't start the challenge more than once.\nIf you need terminate functionality tell me and I'll just kill the script host. - Tesnich"
             #\n!SquatsBot Heathens - Show active squatters! *Maybe lock the door next time...*
             await message.channel.send(response)
 
         #///////////////////////////////////////////////////////////////////////////////////
         #   Subcribe to notifications
-        if message.content == '!SquatsBot Swoletime':
+        if message.content == '!squatsbot signup':
             guildFileName = str(message.guild) +'.csv' #depending on  when server is added file might not exist
             userFound = False
             lines = list()
@@ -101,7 +90,7 @@ class CustomClient(discord.Client):
         
         #///////////////////////////////////////////////////////////////////////////////////
         #   Un-subcribe from notifications
-        if message.content == '!SquatsBot Swolechief Managed':
+        if message.content == '!squatsbot swolechief managed':
             guildFileName = str(message.guild) + '.csv' #depending on  when server is added file might not exist
             userFound = False
             lines = list()
@@ -145,26 +134,73 @@ class CustomClient(discord.Client):
         #     await message.channel.send(response)
 
         #///////////////////////////////////////////////////////////////////////////////////
-        #   Comand to start event notifications
-        if '!SquatsBot Start' in message.content:
+        #   Schedule Event (fill dates)
+        if message.content == '!squatsbot schedule event':
             if (message.author.top_role.permissions.administrator or str(message.author.id) == '234466321465737216'):
-                response = "30 Day Challenge Started! Participants will be at notified every " + str(notificationDelay) + " hours."
+                today = datetime.date.today()
+                now = datetime.datetime.now()
+                startTime = now.strftime("%H:%M:%S")
+                print("Today's date:", today)
+                print("Start Time : ", startTime)
+                dates = list()
+                dates.append('Date')
+                for i in range(30):
+                    dates.append((today + datetime.timedelta(i)).isoformat())
+                print (dates)
+
+                guildFileName = str(message.guild) +'.csv' #depending on  when server is added file might not exist
+
+                lines = list()
+
+                with open (guildFileName, 'r') as csvOut:
+                    fileReader = csv.reader(csvOut)
+                    for row in fileReader:
+                        if(row[0] == 'Date'):
+                            continue
+                        else:
+                            lines.append(row)
+
+                with open (guildFileName, 'w') as csvIn:
+                    fileWriter = csv.writer(csvIn)
+                    for line in lines:
+                        fileWriter.writerow(line)
+                    fileWriter.writerow(dates)
+
+                response = "**New event scheduled from " + dates[1] + " to " + dates[30] + "**. To start notifications use the command '!squatsbot start notify'."
+                await message.channel.send(response)
+
+
+        #///////////////////////////////////////////////////////////////////////////////////
+        #   Comand to start event notifications
+        if '!squatsbot start notify' in message.content:
+            if (message.author.top_role.permissions.administrator or str(message.author.id) == '234466321465737216'):
+                response = "**Participants will be at notified every " + str(notificationDelay) + " hours.**"
                 await message.channel.send(response)
                 client.MentionUsers.start(message)
             else:
-                response = "You must be an admin to use this command :("
+                response = "**You must be an admin to use this command :(**"
                 await message.channel.send(response)
+
+        #///////////////////////////////////////////////////////////////////////////////////
+        #   Comand to end event notifications
+        if '!squatsbot stop notify' in message.content:
+            client.MentionUsers.stop()
+            response = "Notifications have been turned off."
+            await message.channel.send(response)
+
 
     #///////////////////////////////////////////////////////////////////////////////////
     #   Looping task to mention all subscribed users in a daily reminder
     @tasks.loop(hours=notificationDelay)
+    #@tasks.loop(minutes=.1)
     async def MentionUsers(self, message):
         now = datetime.datetime.now()
         print("Mention Users Started ... ", now.strftime("%H:%M:%S"))
 
         today = datetime.date.today().isoformat()
         day = 0
-        standardSquats = 8888
+        datesloaded = False
+        standardSquats = 9999
         hardSquats = 9999
 
         guildFileName = str(message.guild) +'.csv'
@@ -174,11 +210,17 @@ class CustomClient(discord.Client):
                 fileReader = csv.reader(csvOut)
                 for row in fileReader:
                     if(row[0] == 'Date'):
+                        datesloaded = True
                         for item in row:
                             if item == today:
                                 break
                             else:
                                 day += 1
+
+        if datesloaded:
+            with open (guildFileName, 'r') as csvOut:
+                fileReader = csv.reader(csvOut)
+                for row in fileReader:
                     if(row[0] == 'Standard'):
                         standardSquats = row[day]
                     if(row[0] == 'Hard'):
@@ -189,9 +231,14 @@ class CustomClient(discord.Client):
                                 continue
                             else:
                                 userNames += '<@' + item + '> ' 
-
-        await message.channel.send(userNames + 'Welcome to day ' + str(day) + ' of squats! Complete **' + str(standardSquats) + '** squats for the standard challenge, or **'+ str(hardSquats)+ '** for the advanced. Good Luck!')
-        
+            if day > 30:
+                await message.channel.send(userNames + '**Congrats! The squat challenge is Complete!** Thanks For All the Hard Work!')
+                client.MentionUsers.stop()
+            else:
+                await message.channel.send(userNames + 'Welcome to day ' + str(day) + ' of squats! Complete **' + str(standardSquats) + '** squats for the standard challenge, or **'+ str(hardSquats)+ '** for the advanced. Good Luck!')
+        else:
+            await message.channel.send("**Notifications canceled, no active event in database!** Use the command '!sqautsbot schedule event' to start a 30 day course.")
+            client.MentionUsers.stop()
 
 client = CustomClient()
 client.run(TOKEN)
